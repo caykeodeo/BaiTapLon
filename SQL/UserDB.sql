606ab324e866b6ea761845893b72b68ce0862311
caykeodeo <caykeodeo@gmail.com>
2019-12-02 20:47:22 +0700

    Update UserDB.sql
    asd

--- SQL/UserDB.sql
+++ SQL/UserDB.sql
@@ -16,34 +16,3 @@ create table student(
 	foreign key(malop) references [dbo].[classes](malop)
 );
 
-SELECT * FROM [dbo].[classes]
-SELECT * FROM [dbo].[student]
-SELECT * FROM [dbo].account
-DELETE FROM account WHERE username = 'Luc'
-DELETE FROM account
-DELETE FROM [classes]
-DELETE FROM [student]
-SET DATEFORMAT dmy;
-
-
-declare @sql nvarchar(128) =
-	'select *, ''k'' as [' + format(getdate(),'dd/MM/yyyy') + ']
-	FROM [dbo].[students]
-	WHERE [malop] = ?'
-exec(@sql,'DHKHHMT12A')
-
-select format(getdate(),'dd/MM/yyyy')
-
-SELECT *, 'k' as (SELECT(format(getdate(),'dd/MM/yyyy')))
-FROM [dbo].[students]
-WHERE [malop] = 'DHKHMT12A'
-
-SELECT *, flag = k FROM [dbo].[students] WHERE [dbo].[students].malop = 'DHKHMT12A'
-
-insert into [dbo].[classes] values
-('DHKHMT12A',7),
-('DHKTPM12A',8)
-insert into [dbo].[student] values
-('16026501',N'Nguyễn Lê Nhật Quang','DHKHMT12A'),
-('16021821',N'Võ Tấn Lực','DHKHMT12A'),
-('16026801',N'Nguyễn Thành Luân','DHKHMT12A')
